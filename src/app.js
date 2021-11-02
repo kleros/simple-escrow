6f4c56c64a3c539ebdb172ce9e5cdccf1ca21b1b
matt <github.27jtz@aleeas.com>
2021-11-02 14:56:36 -0300

    feat: add deploy handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,71 @@
-import { CHAIN_NAMES, useEtherBalance, useEthers } from "@usedapp/core";
-import { Button, Input, Layout, Typography, Form } from "antd";
+import {
+  CHAIN_NAMES,
+  useEtherBalance,
+  useEthers,
+  useNotifications,
+  useSendTransaction,
+  useTransactions,
+} from "@usedapp/core";
+import {
+  Button,
+  Input,
+  Layout,
+  Typography,
+  Form,
+  notification,
+  List,
+} from "antd";
 import { formatEther } from "@ethersproject/units";
+import { ethers } from "ethers";
+
+import abi from "./assets/abi.json";
+import bytecode from "./assets/bytecode.json";
+import { useEffect, useMemo } from "react";
 
 const { Content } = Layout;
 
+const agreement = "";
+
 function App() {
-  const { chainId, activateBrowserWallet, account } = useEthers();
+  const { chainId, activateBrowserWallet, account, library } = useEthers();
   const etherBalance = useEtherBalance(account);
+  const { notifications } = useNotifications();
+  const { transactions } = useTransactions();
+  const { sendTransaction } = useSendTransaction({
+    transactionName: "Create Escrow",
+  });
 
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const factory = useMemo(() => {
+    if (!account) return;
+    const signer = library.getSigner();
+
+    return new ethers.ContractFactory(abi, bytecode, signer);
+  }, [account, library]);
+
+  const deploy = ({ payee, arbitrator }) => {
+    const deployTx = factory.getDeployTransaction(payee, arbitrator, agreement);
+    sendTransaction(deployTx);
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+  // Notifications.
+  useEffect(() => {
+    notifications.forEach((n) => {
+      if (n.type === "transactionStarted")
+        notification["info"]({
+          key: n.transaction.hash,
+          message: n.transactionName,
+          description: "Waiting for transaction to be mined.",
+          duration: 0,
+        });
+      else if (n.type === "transactionSucceed")
+        notification["success"]({
+          key: n.transaction.hash,
+          message: n.transactionName,
+          description: `Transaction mined. Contract at ${n.transaction.creates}`,
+          duration: 0,
+        });
+    });
+  }, [notifications]);
 
   if (!account)
     return (
@@ -52,14 +103,12 @@ function App() {
       <Typography.Paragraph>
         {etherBalance && <>Balance: {formatEther(etherBalance).slice(0, 5)}</>}
       </Typography.Paragraph>
-
       <Form
         name="create"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
+        onFinish={deploy}
       >
         <Form.Item
           label="Payee"
@@ -88,11 +137,25 @@ function App() {
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" disabled={!account}>
             Deploy
           </Button>
         </Form.Item>
       </Form>
+
+      <List
+        bordered
+        dataSource={transactions}
+        renderItem={(transaction) => (
+          <List.Item>
+            <a
+              href={`https://kovan.etherscan.io/address/${transaction.receipt.contractAddress}`}
+            >
+              {transaction.receipt.contractAddress}
+            </a>
+          </List.Item>
+        )}
+      />
     </Content>
   );
 }
